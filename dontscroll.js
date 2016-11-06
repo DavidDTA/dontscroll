625a3ac40179941cf9a5be2aabcd8ce2d2a03a0e
David Andrews <dta@alumni.cmu.edu>
2016-11-06 01:20:14 -0500

    Be aggressive about not going to the front page

--- dontscroll.js
+++ dontscroll.js
@@ -1,3 +1,9 @@
-if (window.location.pathname == '/') {
-  window.location = 'https://www.messenger.com';
-}
+window.onmousemove = function() {
+  if (window.location.pathname == '/') {
+    window.document.body.remove();
+  }
+};
+window.onmouseup = function() {
+  window.setTimeout(window.onmousemove, 100);
+};
+window.onmousemove();
